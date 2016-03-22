a803fa9f952392b358ca7483cfc38bac182413c8
Xiaodong <Xiaodong@Xiaodong-PC>
2016-03-22 16:32:24 +0800

    添加属性

--- TestDemo/src/yxd/com/service/YxdTest.java
+++ TestDemo/src/yxd/com/service/YxdTest.java
@@ -3,4 +3,28 @@ package yxd.com.service;
 public class YxdTest {
 	private String name;
 	private String Num;
+	private String password;
+	private String sex;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNum() {
+		return Num;
+	}
+	public void setNum(String num) {
+		Num = num;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
 }
